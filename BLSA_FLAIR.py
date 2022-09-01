e93194cd5066e7cad5529a5439c2eeca03b9f6c3
davidkg1 <kendra.g.davidson@vanderbilt.edu>
2022-09-01 16:58:35 -0500

    Added some basic path logic

--- BLSA_FLAIR.py
+++ BLSA_FLAIR.py
@@ -4,3 +4,11 @@ import argparse
 from tqdm import tqdm
 import re
 
+# find current path (assumed to be run in harmonization folder for now)
+currentdir = Path(os.getcwd())
+# find original dataset path
+Old_dir = Path(str(currentdir) + "/BLSA/")
+print(Old_dir)
+#find organized dataset path
+New_dir = Path(str(currentdir) + "/BIDS/BLSA/")
+print(New_dir)
