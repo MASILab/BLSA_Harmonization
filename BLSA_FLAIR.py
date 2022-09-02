002faa93ac5dc9642caf91d12487b89432cdb305
davidkg1 <kendra.g.davidson@vanderbilt.edu>
2022-09-02 14:33:33 -0500

    Old code, expanded to include PDs and T2s, changed filename

--- BLSA_FLAIR.py
+++ BLSA_FLAIR.py
@@ -3,6 +3,7 @@ import os
 import argparse
 from tqdm import tqdm
 import re
+import glob
 
 # find current path (assumed to be run in harmonization folder for now)
 currentdir = Path(os.getcwd())
@@ -22,6 +23,37 @@ for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     splits = b.name.split('_')
     subj_id = "sub-" + "".join(splits[:2])
     sess_id = "ses-" + "".join(splits[2].split('-')) + "scanner" + splits[3]
-    print(subj_id, sess_id)
+    print("\n",subj_id, sess_id)
     if not scans_path.exists():
         continue
+
+#get FLAIR
+    flair = Path(str(scans_path) + "/FLAIR/NIFTI/")
+    # print(flair)
+    if not flair.exists(): continue
+    try:
+        fn = [x for x in flair.glob('*FLAIR.ni*')]
+        for f in fn: print(str(f))
+        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+    except:
+        print("echo 'Error: no flair for {},{}'".format(subj_id, sess_id))
+
+#get PD
+    pd = Path(str(scans_path) + "/PD/NIFTI")
+    if not pd.exists(): continue
+    try:
+        pdn = [x for x in pd.glob('*PD.ni*')]
+        for f in pdn: print(str(f))
+        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+    except:
+        print("echo 'Error: no pd for {},{}'".format(subj_id, sess_id))
+
+#get T2
+    t2 = Path(str(scans_path) + "/T2/NIFTI")
+    if not t2.exists(): continue
+    try:
+        t2n = [x for x in t2.glob('*T2.ni*')]
+        for f in t2n: print(str(f))
+        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+    except:
+        print("echo 'Error: no t2 for {},{}'".format(subj_id, sess_id))
