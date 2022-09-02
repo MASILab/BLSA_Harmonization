309e7c8ea4808f08b22b492ae89d78e2a46cbdcf
davidkg1 <kendra.g.davidson@vanderbilt.edu>
2022-09-02 15:58:04 -0500

    Now prints out bash commands to create symlinks

--- BLSA_Symlinks.py
+++ BLSA_Symlinks.py
@@ -5,16 +5,18 @@ from tqdm import tqdm
 import re
 import glob
 
+def Create_Bash_Line(ogpath, newpath):
+    return "ln -s " + str(ogpath) + " " + str(newpath)
+
 # find current path (assumed to be run in harmonization folder for now)
 currentdir = Path(os.getcwd())
 # find original dataset path
 Old_dir = Path(str(currentdir) + "/BLSA/")
-print(Old_dir)
+# print(Old_dir)
 #find organized dataset path
 New_dir = Path(str(currentdir) + "/BIDS/BLSA/")
-print(New_dir)
+# print(New_dir)
 
-scan_types = ["FLAIR", "T2", "PD"]
 sub_pattern = re.compile('BLSA_[0-9]{4}_[0-9]{2}-[0-9]_[0-9]{2}$')
 for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     #path to the raw scans, no accessors
@@ -23,7 +25,7 @@ for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     splits = b.name.split('_')
     subj_id = "sub-" + "".join(splits[:2])
     sess_id = "ses-" + "".join(splits[2].split('-')) + "scanner" + splits[3]
-    print("\n",subj_id, sess_id)
+    # print("\n",subj_id, sess_id)
     if not scans_path.exists():
         continue
 
@@ -33,8 +35,9 @@ for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     if not flair.exists(): continue
     try:
         fn = [x for x in flair.glob('*FLAIR.ni*')]
-        for f in fn: print(str(f))
-        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+        for f in fn: str(f)
+        SymlinkPath = str(New_dir) + "/" + subj_id + "/" + sess_id + "/anat/"+  subj_id + '_' + sess_id +'_FLAIR' + ".nii.gz"
+        print(Create_Bash_Line(f,SymlinkPath))
     except:
         print("echo 'Error: no flair for {},{}'".format(subj_id, sess_id))
 
@@ -43,8 +46,9 @@ for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     if not pd.exists(): continue
     try:
         pdn = [x for x in pd.glob('*PD.ni*')]
-        for f in pdn: print(str(f))
-        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+        for p in pdn: str(p)
+        SymlinkPath = str(New_dir) + "/" + subj_id + "/" + sess_id + "/anat/"+  subj_id + '_' + sess_id +'_PD' + ".nii.gz"
+        print(Create_Bash_Line(p,SymlinkPath))
     except:
         print("echo 'Error: no pd for {},{}'".format(subj_id, sess_id))
 
@@ -53,7 +57,8 @@ for b in tqdm([x for x in Old_dir.glob('BLSA_*') if sub_pattern.match(x.name)]):
     if not t2.exists(): continue
     try:
         t2n = [x for x in t2.glob('*T2.ni*')]
-        for f in t2n: print(str(f))
-        # print(get_bash_line(BIDS_path, subj_id, sess_id, mpr))
+        for t in t2n: str(f)
+        SymlinkPath = str(New_dir) + "/" + subj_id + "/" + sess_id + "/anat/"+  subj_id + '_' + sess_id +'_T2w' + ".nii.gz"
+        print(Create_Bash_Line(t,SymlinkPath))
     except:
         print("echo 'Error: no t2 for {},{}'".format(subj_id, sess_id))
